8bf0230e85cb7add326fe32accafb93cf3379077
starprince7 <princeagezinweke@gmail.com>
2022-10-02 05:59:34 +0800

    feat: api list of product category

---
+++ pages/api/categories/[categoryName].ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import Product from "../../../database/models/productSchema";
+import db from "../../../database/dbUtils/dbConnection";
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "GET") {
+    res.status(400);
+    res.json({ msg: "Bad request" });
+    return;
+  }
+
+  // Connect DB
+  await db.connectDB();
+
+  const { categoryName } = req.query;
+
+  const products = await Product.find({ category: categoryName });
+  res.status(200);
+  res.json(products);
+  res.end();
+}
