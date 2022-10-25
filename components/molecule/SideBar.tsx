6fcc966f53173793bfba0eb94bbd6159da9a6607
Udoka <udochukwukaonyela@gmail.com>
2022-10-25 17:35:18 +0100

    created sidebar component

---
+++ components/molecule/SideBar.tsx
@@ -0,0 +1,13 @@
+import  React, { FC } from 'react';
+
+export interface ISideBarProps {
+}
+
+const SideBar: FC<ISideBarProps> = (props) => {
+  return (
+    <section>
+      SideBar
+    </section>
+  );
+}
+export default SideBar;
