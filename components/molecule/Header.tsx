d59a970168784ac96f7a499978229e73bb436dbf
Udoka <udochukwukaonyela@gmail.com>
2022-10-25 17:34:58 +0100

    Created Header component

---
+++ components/molecule/Header.tsx
@@ -0,0 +1,13 @@
+import  React, { FC } from 'react';
+
+export interface IHeaderProps {
+}
+
+const Header: FC<IHeaderProps> = (props) => {
+  return (
+    <section>
+      Header
+    </section>
+  );
+}
+export default Header;
