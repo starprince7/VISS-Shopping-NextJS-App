6f27abf28f6bc7e83b8d081871756067b07b0437
Udoka <udochukwukaonyela@gmail.com>
2022-10-25 17:34:49 +0100

    created SideLink component

---
+++ components/atom/SideLink.tsx
@@ -0,0 +1,13 @@
+import  React, { FC } from 'react';
+
+export interface ISideLinkProps {
+}
+
+const SideLink: FC<ISideLinkProps> = (props) => {
+  return (
+    <section>
+      SideLink
+    </section>
+  );
+}
+export default SideLink;
