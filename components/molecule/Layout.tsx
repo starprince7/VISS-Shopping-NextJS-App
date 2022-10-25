f0ee58c90c17f47c6a4e5a4122978dc3a16f46bf
Udoka <udochukwukaonyela@gmail.com>
2022-10-25 17:35:12 +0100

    Created Layout comppnent

---
+++ components/molecule/Layout.tsx
@@ -0,0 +1,13 @@
+import  React, { FC } from 'react';
+
+export interface ILayoutProps {
+}
+
+const Layout: FC<ILayoutProps> = (props) => {
+  return (
+    <section>
+      Layout
+    </section>
+  );
+}
+export default Layout;
