f21377e34c01b7b5c0705cd5f2bd93bef60fa991
Udoka <udochukwukaonyela@gmail.com>
2022-10-25 17:35:26 +0100

    created dashboard page

---
+++ pages/admin/Dashboard.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+export interface IDashboardProps {
+}
+
+const Dashboard: NextPage<IDashboardProps> = () => {
+  return (
+    <section>
+      Dashboard
+    </section>
+  );
+}
+export default Dashboard;
