aebff88da5e0f8fd448f88e83cd65bdc5a4fe8fd
Udoka <udochukwukaonyela@gmail.com>
2022-10-25 17:18:50 +0100

    fixes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /**
  * @type {import('next').NextConfig}
  */
- const nextConfig = {
+const nextConfig = {
     /* config options here */
-  }
-  
-  module.exports = nextConfig
+};
+
+module.exports = nextConfig;
