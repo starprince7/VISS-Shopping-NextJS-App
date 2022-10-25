676634ab58b7dbf007f35a20881f4dfaf549aebd
Udoka <udochukwukaonyela@gmail.com>
2022-10-25 17:41:34 +0100

    fixed template

--- template/icon.tsx
+++ template/icon.tsx
@@ -1,7 +1,4 @@
 import React, { FC } from 'react';
-import type { IconProps, Props, PropTypes } from "../../typescript";
-
-
 
 const blockName: FC<PropTypes<Props, IconProps>> = (props) => {
     return (
