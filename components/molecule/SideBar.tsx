6204983bc455874dcacf6316ab16756b63380e70
Udoka <udochukwukaonyela@gmail.com>
2022-10-31 05:07:22 +0100

    fixed svg props

--- components/molecule/SideBar.tsx
+++ components/molecule/SideBar.tsx
@@ -1,10 +1,43 @@
 import  React, { FC } from 'react';
+import { Products, Dashboard, Customers, ClipboardCopy, ClipboardCheck } from "../../assets/icons";
 
 export interface ISideBarProps {
   className: string;
 }
-
-const SideBar: FC<ISideBarProps> = ({className}) => {
+interface ILinks {
+  to: string;
+  text: string;
+  icon: FC;
+}
+const links = [
+  {
+    to: "/admin/dashboard",
+    text: "Dashboard",
+    icon: Dashboard,
+  },
+  {
+    to: "/admin/products",
+    text: "Products",
+    icon: Products,
+  },
+  {
+    to: "/admin/customers",
+    text: "Customers",
+    icon: Customers,
+  },
+  {
+    to: "/admin/orders",
+    text: "Orders",
+    icon: ClipboardCopy,
+  },
+  {
+    to: "/admin/fullfilledorders",
+    text: "Fullfilled Orders",
+    icon: ClipboardCheck,
+  }
+]
+const SideBar: FC<ISideBarProps> = ({ className }) => {
+  
   return (
     <section className={`${className}`}>
       SideBar
