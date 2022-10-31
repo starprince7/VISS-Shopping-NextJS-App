15c3651ddd0a8d7f59767e4e66a117fa712c9611
Udoka <udochukwukaonyela@gmail.com>
2022-10-31 06:09:40 +0100

    fixes

--- pages/admin/dashboard.tsx
+++ pages/admin/dashboard.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React from 'react';
 
 import { NextPage } from 'next';
 
 import { Layout } from '../../components/molecule';
 
 const dashboard: NextPage = () => {
-  return <Layout className="bg-gray">dashboard</Layout>;
+  return (
+    <Layout className="bg-background">
+      <div>
+        <h1>dashboard</h1>
+      </div>
+    </Layout>
+  );
 };
 export default dashboard;
