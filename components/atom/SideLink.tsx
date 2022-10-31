1f1ad1877300aeeabb8f7dbdc3899a3377f8f4a4
Udoka <udochukwukaonyela@gmail.com>
2022-10-31 06:09:38 +0100

    fixes

--- components/atom/SideLink.tsx
+++ components/atom/SideLink.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode } from 'react';
+import React, { FC } from 'react';
 
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -14,7 +14,7 @@ const SideLink: FC<ISideLinkProps> = ({ to, text, Icon }) => {
   return (
     <Link href={to}>
       <div className={`${router.pathname === to ? 'bg-linkBg text-primary' : 'text-gray'} hover:text-primary hover:bg-linkBg transition-colors ease-in duration-500 flex gap-4 items-center py-4 px-3 rounded-md cursor-pointer group`}>
-        <Icon className="group-hover:fill-primary fill-gray" />
+        <Icon className={`group-hover:fill-primary  ${router.pathname === to ? 'fill-primary' : 'fill-gray'} transition-colors ease-in duration-500`} />
         <span className="text-base font-medium">{text}</span>
       </div>
     </Link>
