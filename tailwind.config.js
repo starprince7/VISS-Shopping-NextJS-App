7efd5ce498f4e4d7c9a3dfeb90ba0a7d7cb5f7f4
Udoka <udochukwukaonyela@gmail.com>
2022-10-31 06:00:39 +0100

    added new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
             colors: {
                 primary: "#89A67E",
                 background: "#E8E8E8",
+                linkBg: "rgba(137, 166, 126, 0.12)",
+                gray: "#9C9C9C",
             }
         }
     },
