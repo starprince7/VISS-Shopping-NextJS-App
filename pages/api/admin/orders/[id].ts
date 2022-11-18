623d2aefd634b7ec982003f86b1df3f15165a420
starprince7 <princeagezinweke@gmail.com>
2022-11-18 21:43:36 +0800

    refactor: api route refactor

--- pages/api/admin/orders/[id].ts
+++ pages/api/admin/orders/[id].ts
@@ -8,34 +8,35 @@ import db from "../../../../database/dbUtils/dbConnection";
 import Orders from "../../../../database/models/orderSchema";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const { method } = req;
-    await db.connectDB();
+  const { method } = req;
+  await db.connectDB();
 
-    // Grab Order ID
-    const orderId = req.query.id;
+  // Grab Order ID
+  const orderId = req.query.id;
 
-    switch (method) {
-        case "GET":
-            const orderDetails = await Orders.findById(orderId);
-            if (!orderDetails) {
-                return res.status(404).json({ msg: "Order was not found" });
-            }
+  let orderDetails, deletedOrder;
+  switch (method) {
+    case "GET":
+      orderDetails = await Orders.findById(orderId);
+      if (!orderDetails) {
+        return res.status(404).json({ msg: "Order was not found" });
+      }
+      res.status(200).json(orderDetails);
+      break;
 
-            res.status(200).json(orderDetails);
-            break;
-        case "DELETE":
-            const deletedOrder = await Orders.findByIdAndDelete(orderId);
-            if (!deletedOrder) {
-                return res.status(404).json({ msg: "Order was not found" });
-            }
+    case "DELETE":
+      deletedOrder = await Orders.findByIdAndDelete(orderId);
+      if (!deletedOrder) {
+        return res.status(404).json({ msg: "Order was not found" });
+      }
+      res.status(200).json({
+        msg: "Order deleted successfully",
+        order: deletedOrder,
+      });
+      break;
 
-            res.status(200).json({
-                msg: "Order deleted successfully",
-                order: deletedOrder,
-            });
-            break;
-        default:
-            res.status(405).json({ error: "Method not allowed" });
-            break;
-    }
+    default:
+      res.status(405).json({ error: "Method not allowed" });
+      break;
+  }
 };
--- pages/api/search/index.ts
+++ pages/api/search/index.ts
@@ -3,22 +3,24 @@ import db from "../../../database/dbUtils/dbConnection";
 import Product from "../../../database/models/productSchema";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-    const { method } = req;
-    await db.connectDB();
-    
-    const { query } = req.body;
-    switch (method) {
-        case "POST":
-            if (!query) {
-                return res.status(401).json({ error: "No query parameter found" });
-            }
-            const results = Product.find(
-                { $text: { $search: query } },
-            ).sort({ score: { $meta: "textScore" } });
-            res.status(200).json({ data: results });
-            break;
-        default:
-            res.status(405).json({ error: "Method not allowed" });
-            break;
-    }
+  const { method } = req;
+  await db.connectDB();
+
+  const { query } = req.body;
+
+  switch (method) {
+    case "POST":
+      if (!query) {
+        return res.status(401).json({ error: "No query parameter found" });
+      }
+      const results = Product.find({ $text: { $search: query } }).sort({
+        score: { $meta: "textScore" },
+      });
+      res.status(200).json({ data: results });
+      break;
+
+    default:
+      res.status(405).json({ error: "Method not allowed" });
+      break;
+  }
 }
